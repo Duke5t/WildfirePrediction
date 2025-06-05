b6b981ad5549c4e4a26295ab176a216fee07bbb0
Duke5t <68446366+Duke5t@users.noreply.github.com>
2025-06-05 08:29:32 -0500

    Add files via upload

--- XGBoost.py
+++ XGBoost.py
@@ -1,9 +1,28 @@
-
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class XGBoost:
 
 
     def __init__(self, df, quantFeat, catFeat, predFeat):
-        pass
+        
+        self.df = df.copy()
+        selectedCols = quantFeat + catFeat
+
+        for col in catFeat:
+            self.df[col] = self.df[col].astype('category')
+            
     
+        fireX = self.df[selectedCols]
+        fireY = self.df[predFeat]
+
+        X_train, X_test, y_train, y_test = train_test_split(fireX, fireY, test_size=0.2)
+
+
+        
+        pass
